bc4f3acefe0f5fc54cb434d7ea062c3e0e7ec837
Error-Mozz13 <mozz7e6@gmail.com>
2023-09-08 15:08:59 -0700

    Solved warmup-1/parrot_trouble

--- warmup-1/parrot_trouble/answer_1.py
+++ warmup-1/parrot_trouble/answer_1.py
@@ -1,2 +1,12 @@
+# Takes (bool talking, int hour) input, 
+# task: if talking and (hour < 7 or hour > 20) return true
 def parrot_trouble(talking, hour):
-    
+    return talking and (hour < 7 or hour > 20)
+
+if __name__ == "__main__":
+    # Testing with different inputs and expected results
+    print(parrot_trouble(True, 6) == True)
+    print(parrot_trouble(True, 7) == False)
+    print(parrot_trouble(False, 6) == False)
+    print(parrot_trouble(True, 20) == False)
+    print(parrot_trouble(True, 21) == True)
