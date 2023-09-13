c3e0077496f62db4823ecb786dbdf0c8e36d5342
Error-Mozz13 <mozz7e6@gmail.com>
2023-09-12 23:47:02 -0700

    Solved string-2/double_char

---
+++ string-2/double_char/answer_1.py
@@ -0,0 +1,19 @@
+"""
+Takes (str str) input, 
+task: return a new string where there's an additional duplicate character in place of each original character
+"""
+def double_char(str):
+  result = ""
+  for char in str:
+    result += char * 2 
+  return result
+
+if __name__ == "__main__":
+  # Testing with different inputs and expected results
+  print(double_char('The') == 'TThhee')
+  print(double_char('AAbb') == 'AAAAbbbb')
+  print(double_char('Hi-There') == 'HHii--TThheerree')
+
+  print(double_char('The'))
+  print(double_char('AAbb'))
+  print(double_char('Hi-There'))
