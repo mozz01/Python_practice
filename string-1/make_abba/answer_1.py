42e1ab73d6e6bc51c5913a9672ded53d815b93f0
Error-Mozz13 <mozz7e6@gmail.com>
2023-09-12 22:45:10 -0700

    Renamed directories

---
+++ string-1/make_abba/answer_1.py
@@ -0,0 +1,13 @@
+"""
+Takes (str a, str b) input, 
+task:
+"""
+def make_abba(a, b):
+  return 
+
+if __name__ == "__main__":
+  # Testing with different inputs and expected results
+  print(make_abba('Hi', 'Bye') == 'HiByeByeHi')
+  print(make_abba('Yo', 'Alice') == 'YoAliceAliceYo')
+  print(make_abba('What', 'Up') == 'WhatUpUpWhat')
+  
