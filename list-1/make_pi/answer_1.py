fa2a828608fe4dd581e7374f44be3c2fac287068
Error-Mozz13 <mozz7e6@gmail.com>
2023-09-18 13:25:45 -0700

    Solved list-1/make_pi

---
+++ list-1/make_pi/answer_1.py
@@ -0,0 +1,11 @@
+"""
+Takes () input, 
+task: Return [3, 1, 4] list
+"""
+def make_pi():
+  return [3, 1, 4]
+
+if __name__ == '__main__':
+  # Testing input and expected result
+  print(make_pi() == [3, 1, 4])
+  
